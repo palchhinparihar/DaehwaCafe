a51357a928c076b5282fa22d0f58f4fd80f90905
palchhin <palchhinparihar@gmail.com>
2026-07-09 16:45:18 +0530

    update: added condense packages for chunking process

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,43 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    visualizer({
-      open: true,
-      gzipSize: true,
-      brotliSize: true,
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (!id.includes("node_modules")) return;
+
+          if (
+            id.includes("/react/") ||
+            id.includes("react-dom") ||
+            id.includes("react-router-dom")
+          ) {
+            return "react-vendor";
+          }
+
+          if (id.includes("framer-motion")) {
+            return "motion";
+          }
+
+          if (id.includes("react-icons")) {
+            return "icons";
+          }
+
+          if (id.includes("swiper")) {
+            return "swiper";
+          }
+
+          if (id.includes("aos")) {
+            return "aos";
+          }
+
+          return "vendor";
+        },
+      },
+    },
+  },
 });
